7f2ad001ca1e0a66ec38a6b56ecf8fac940b1ebe
MrJouH4 <yousefhesham1235@gmail.com>
2024-02-02 17:14:59 +0200

    Deblur training added

---
+++ src/deblur_train.py
@@ -0,0 +1,49 @@
+import torch
+from Utils.addToExcelsheet import add_to_excelsheet
+
+def train(model, optimizer, criterion,gopro_dataloader_train, folder, checkpoint_path, excel_file, progress_file, device="cuda", lr=None):
+    checkpoint = torch.load(checkpoint_path, map_location=torch.device(device))
+    model.load_state_dict(checkpoint['model_state_dict'])
+    device = torch.device(device)
+    epoch = int(checkpoint_path.split("/")[-1].split("_")[-1][:-4])
+    print(epoch)
+    i = 0
+    num_epochs = epoch + 2
+    with open(progress_file, "w") as file:
+        for epoch in range(epoch + 1, num_epochs):
+            i = 0
+            batch_loss = []
+            for batch_no, (noisy_images, gt_images) in enumerate(gopro_dataloader_train):
+                patches_loss = []
+                for patch_index in range(noisy_images[0].size(0)):
+                    noisy_image = noisy_images[:, patch_index].to(device)
+                    gt_image = gt_images[:, patch_index].to(device)
+                    optimizer.zero_grad()
+                    outputs = model(noisy_image)
+                    loss = criterion(outputs, gt_image)
+                    loss.backward()
+                    optimizer.step()
+                    patches_loss.append(loss.item())
+                    progress_str = f"Epoch [{epoch}], Step [{batch_no+1}/{len(gopro_dataloader_train)}], Patch [{patch_index+1}/{noisy_images[0].size(0)}], Loss: {loss.item()}"
+                    print(progress_str)
+                    file.write(progress_str + "\n")
+                batch_loss.append(sum(patches_loss) / len(patches_loss))
+            i += 1
+            if i % 200 == 0 and i != 0:
+                    torch.save({
+                            'model_state_dict': model.state_dict(),
+                            'optimizer_state_dict': optimizer.state_dict(),
+                        }, f"{folder}/GoPro_epoch_{epoch}.pth")
+                    progress_str = f"Epoch [{epoch}], Epoch Loss: {sum(batch_loss) / len(batch_loss)}"
+                    print(progress_str)
+                    file.write(progress_str + "\n")
+            torch.save({
+                    'model_state_dict': model.state_dict(),
+                    'optimizer_state_dict': optimizer.state_dict(),
+                }, f"{folder}/GoPro_epoch_{epoch}.pth")
+            Epoch_loss = sum(batch_loss) / len(batch_loss)
+            progress_str = f"Epoch [{epoch}], Epoch Loss: {Epoch_loss}"
+            print(progress_str)
+            add_to_excelsheet(excel_file,epoch,0, Epoch_loss,lr)
+            file.write(progress_str + "\n")
+    file.close()
